47d5c27ab6f2f16b16f2c9a203f0d0822b3b2168
unknown <muriloaleme17@gmail.com>
2026-01-17 00:04:56 -0300

    'first-commit'

---
+++ src/main/java/com/fiap/sus_triage/SusTriageApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.sus_triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusTriageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/sus_triage/SusTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.sus_triage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusTriageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
