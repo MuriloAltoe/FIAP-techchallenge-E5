db362da509664d0dd9519c4d2645a7a95b33dbde
unknown <muriloaleme17@gmail.com>
2026-02-02 22:58:43 -0300

    [updt]: collections-update&&default-data-add

---
+++ src/main/resources/data.sql
@@ -0,0 +1,32 @@
+INSERT INTO patient (name, gender, age)
+VALUES ('João da Silva', 'M', 34);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Maria Santos', 'F', 39);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Carlos Oliveira', 'M', 32);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Ana Costa', 'F', 36);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Pedro Ferreira', 'M', 29);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Juliana Martins', 'F', 33);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Roberto Alves', 'M', 37);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Fernanda Gomes', 'F', 31);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Lucas Pereira', 'M', 35);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Beatriz Sousa', 'F', 30);
+
+INSERT INTO patient (name, gender, age)
+VALUES ('Gustavo Ribeiro', 'M', 38);
